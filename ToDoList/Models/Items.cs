a35443c004486ff243f77c71fcc52d5d9d377064
Student <example@example.com>
2019-10-01 10:00:48 -0700

    Complete test logic for list test

--- ToDoList/Models/Items.cs
+++ ToDoList/Models/Items.cs
@@ -1,9 +1,24 @@
+using System.Collections.Generic;
 namespace ToDoList.Models
 {
     public class Item
     {
+        // We add set; to the line below.
+        public string Description { get; set; }
+        private static List<Item> _instances = new List<Item> { };
 
-        // properties, methods, etc. will go here.
-
+        public Item(string description)
+        {
+            Description = description;
+            _instances.Add(this);
+        }
+        public static List<Item> GetAll()
+        {
+            return _instances;
+        }
+        public static void ClearAll()
+        {
+            _instances.Clear();
+        }
     }
 }
---
+++ ToDoList/Models/Programs.cs
@@ -0,0 +1,24 @@
+// using System;
+// using System.Collections.Generic;
+// using ToDoList.Models;
+
+// namespace ToDoList
+// {
+//     public class Program
+//     {
+//         static void Main()
+//         {
+//             Console.WriteLine("Wellcome to your To Do List! Would you like to 'add' items to your list, 'view' list or 'update' list?");
+
+//             string viewAddUpdate = Console.ReadLine();
+
+//             if(viewAddUpdate.ToLower == "add")
+//             {
+                
+//             }
+//         }
+
+//     }
+
+// }
+
